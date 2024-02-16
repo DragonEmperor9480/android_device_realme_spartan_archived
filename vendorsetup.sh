7da096d4d8fb5ccda0db130f2c48b71316dc19e5
DragonEmperor9480 <amruteshnaregal1234@gmail.com>
2024-02-16 21:30:08 +0530

    Revert "spartan: vendorsetup: update kernel link"
    This reverts commit 8402e8d3f745cc5f58be0ce1e481cd8ce7b23ed8.

--- vendorsetup.sh
+++ vendorsetup.sh
@@ -2,7 +2,7 @@ echo 'Cloning vendor'
 git clone https://github.com/Dragon-s-Playground/android_vendor_realme_spartan.git -b dragon-test vendor/realme/spartan
 
 echo "Cloning kernel"
-git clone https://github.com/Dragon-s-Playground/pikachu_kernel_rmx3371.git -b pikachu-AOSP kernel/realme/sm8250 --depth=1
+git clone https://github.com/DragonEmperor9480/rmx3371_kernel.git kernel/realme/sm8250 --depth=1
 
 echo "Cloning oplus hals"
 git clone https://github.com/DragonEmperor9480/android_hardware_oplus_rmx3371.git hardware/oplus -b test-14
